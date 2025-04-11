8dedfa945b822fc5ce8e396529bad967ad27d722
pyloris <shoaibwani@protonmail.com>
2025-04-11 14:57:13 +0200

    fix: fixed the controller

--- controllers/handler.controller.js
+++ controllers/handler.controller.js
@@ -1,8 +1,6 @@
 const fetchCommands = require("../helpers/fetchCommands");
 const resultService = require("../services/results.service");
 
-var timeoutId;
-
 const getResults = async (req, res) => {
     const results = await resultService.get();
 
@@ -13,23 +11,9 @@ const storeCommands = async (req, res) => {
     const commands = req.body;
 
     res.json({ status: "success", message: "commands recieved." });
-
-    if (timeoutId) clearInterval(timeoutId);
     
     // done executing.
     await fetchCommands(commands.commands);
-
-    if (await resultService.isempty()) {
-        process.exit(0);
-    }
-
-	console.log("FINISHED EXECUTION: kill in 5MINS");
-
-    timeoutId = setTimeout(() => {
-        process.exit(0);
-    }, 1000*60*59);
-
-    return;
 };
 
 
